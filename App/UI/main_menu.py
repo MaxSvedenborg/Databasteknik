9b62e2de7ab8ab38431459a0571c247e9f3ae278
Alicia Chumchai Toom <alicia.chumchai@gmail.com>
2020-11-23 07:09:57 +0100

    Add main memu.py

--- App/UI/main_menu.py
+++ App/UI/main_menu.py
@@ -6,13 +6,29 @@ def main_menu():
         print("Main Menu")
         print("=========")
         print("1. Customers")
-        print("2. Products")
-        print("3. Orders")
-        print("4. Quit")
+        print("2. Customer Types")
+        print("3. Cars")
+        print("4. Orders")
+        print("5. Personal Data")
+        print("6. Employees")
+        print("7. Cars Spareparts")
+        print("8. Orders Spareparts")
+        print("9. Suppliers")
+        print("10. Store")
+        print("11. Manufacturer")
+        print("12. Spareparts")
+        print("13. Inventories")
+        print("14. Quit")
 
         selection = input("> ")
         if selection == "1":
             customers_menu()
+        elif selection == "2":
+            customers_type_menu()
+        elif selection == "3":
+            cars_menu()
+        elif selection == "4":
+            orders_menu()
 
         else:
             break
