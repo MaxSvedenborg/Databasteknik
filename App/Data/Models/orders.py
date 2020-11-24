09f6b6fdbd7e02e24615a7068eaad55ee9b407bd
Alicia Chumchai Toom <alicia.chumchai@gmail.com>
2020-11-24 15:40:51 +0100

    Add final customer UI, BL, Repository

--- App/Data/Models/orders.py
+++ App/Data/Models/orders.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 class Order(Base):
     __tablename__ = 'orders'
 
-    OrdersId = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    OrderId = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     OrderDate = sa.Column(sa.Date, nullable=False)
     OrderTime = sa.Column(sa.Time, nullable=False)
     StoreId = sa.Column(sa.Integer, sa.ForeignKey('stores.StoreId'), nullable=False)
